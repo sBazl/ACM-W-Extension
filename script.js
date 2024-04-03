fc3e7a6dc2334eb19887dad755e37729e1f233ae
sBazl <118675052+sBazl@users.noreply.github.com>
2024-04-03 16:50:41 -0400

    final js

--- script.js
+++ script.js
@@ -6,16 +6,14 @@
  * Which data structure stores data in order and stored contiguously in memory?
  *  */ 
 
-//----replace with break storage var--//
-
+const allbreaks = [];
 /**
  * Create a variable of the appropriate type to contain all the user dates and their corressponding title
  * 
  * Which data structure stores data in pairs?
  *  */ 
 
-//----replace with break/title pairing var----//
-
+const breakMap = new Map();
 
 
 //-----------------------------FUNCTIONS------------------------------------------//
@@ -29,14 +27,24 @@ function saveAddedDate() {
 
 
     // STEP 1: get the user input string yyyy-mm-dd and store in a var
+    let breakTitle = document.getElementById("breakName").value;
     // STEP2: get the user input string and store in a var
-
+    let dateString = document.getElementById("newDate").value;
     //STEP 3: convert the datestring into a date object and save in a var
     
-    // STEP 4: add date obj to storage data structure (defined in the variables section)
-    // STEP 5: store date : title pairing (hint: use the set method)
+    let newDate = new Date(dateString);
+    console.log("saved date", dateString)
 
+    // STEP 4: add date obj to storage data structure (defined in the variables section)
+    allbreaks.push(newDate);
+    console.log("the array length", allbreaks.length)
+    // STEP 5: store date : title pairing (hint: use the set method)
+    breakMap.set(newDate, breakTitle);
     //STEP 6: Clear the inputs and remove the input form from dom
+
+    document.getElementById("breakName").value = "";
+    document.getElementById("breakName").value = "";
+    document.getElementById("input").style.display = "none"
 
     initTimer() // call to start the timer
 }
@@ -46,7 +54,16 @@ function saveAddedDate() {
  *  
  * */
 function showInputForm() {
+    const inputForm = document.getElementById("input")
     //HINT: use the style.display property of the element
+    if (inputForm.style.display === "none") {
+        inputForm.style.display = "block";
+      } else {
+        inputForm.style.display = "none";
+
+      }
+
+
     
 
 }
@@ -55,53 +72,53 @@ function showInputForm() {
  * This function is responsible for finding the next closest date that the user added.
  */
 
-// function initTimer() {
+function initTimer() {
 
 
-//     if (allBreaks.length != 0) {
-//         allBreaks.sort(); //sort the array
+    if (allbreaks.length != 0) {
+        allbreaks.sort(); //sort the array
 
 
-//         // Update the count down every 1 second
-//         var x = setInterval( function() {
-//             var today = new Date() //local time ( 5 hrs behind utc)
-//             var todayUTC = new Date(today.getTime() - today.getTimezoneOffset() * 60000); // convert local to utc
+        // Update the count down every 1 second
+        var x = setInterval( function() {
+            var today = new Date() //local time ( 5 hrs behind utc)
+            var todayUTC = new Date(today.getTime() - today.getTimezoneOffset() * 60000); // convert local to utc
 
 
-//             var upcomingBreak = allBreaks.find( date => date > todayUTC);
+            var upcomingBreak = allbreaks.find( date => date > todayUTC);
 
-//         // Find the distance between now and the count down date
+        // Find the distance between now and the count down date
 
-//         if (upcomingBreak) {
-//             var distance = upcomingBreak - todayUTC; //in millisecs
-//             // Time calculations for days, hours, minutes and seconds
-//             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        if (upcomingBreak) {
+            var distance = upcomingBreak - todayUTC; //in millisecs
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//             // Display the result in the element with id="demo"
-//             document.getElementById("break").innerHTML = breaksMap.get(upcomingBreak); //should return the val of the break's name
+            // Display the result in the element with id="demo"
+            document.getElementById("break").innerHTML = breakMap.get(upcomingBreak); //should return the val of the break's name
 
-//             document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-//             + minutes + "m " + seconds + "s ";
+            document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
 
-//         } else {
-//             // Handle the case where there's no upcoming break
-//             document.getElementById("timer").innerHTML = "No Breaks Yet :(";
-//             clearInterval(x); // Optionally, clear the interval if no upcoming breaks
-//         }
+        } else {
+            // Handle the case where there's no upcoming break
+            document.getElementById("timer").innerHTML = "No Breaks Yet :(";
+            clearInterval(x); // Optionally, clear the interval if no upcoming breaks
+        }
        
         
-//         if (distance < 0) {
-//         clearInterval(x);
-//         document.getElementById("timer").innerHTML = "WOOOOOO!";
-//         }
-//     }, 1000);
+        if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "WOOOOOO!";
+        }
+    }, 1000);
 
-//     }
-//     console.log ('list', allBreaks.length)
-// }
+    }
+    console.log ('list', allBreaks.length)
+}
 
 //-----------------FUNCTION CALLS---------------------------------------
 document.getElementById("saveBtn").onclick = function() {saveAddedDate()};
